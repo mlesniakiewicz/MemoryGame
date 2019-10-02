22f05a7a0e739552c0b9d37f9774045d1cda929e
Michal L <michal.lesniakiewicz@gmail.com>
2019-10-02 12:40:34 +0200

    ads

---
+++ memory.js
@@ -0,0 +1,129 @@
+var cards = [
+  "ciri.png",
+  "geralt.png",
+  "jaskier.png",
+  "jaskier.png",
+  "iorweth.png",
+  "triss.png",
+  "geralt.png",
+  "yen.png",
+  "ciri.png",
+  "triss.png",
+  "yen.png",
+  "iorweth.png"
+];
+
+var c0 = document.getElementById("c0");
+var c1 = document.getElementById("c1");
+var c2 = document.getElementById("c2");
+var c3 = document.getElementById("c3");
+
+var c4 = document.getElementById("c4");
+var c5 = document.getElementById("c5");
+var c6 = document.getElementById("c6");
+var c7 = document.getElementById("c7");
+
+var c8 = document.getElementById("c8");
+var c9 = document.getElementById("c9");
+var c10 = document.getElementById("c10");
+var c11 = document.getElementById("c11");
+
+c0.addEventListener("click", function() {
+  revealCard(0);
+});
+c1.addEventListener("click", function() {
+  revealCard(1);
+});
+c2.addEventListener("click", function() {
+  revealCard(2);
+});
+c3.addEventListener("click", function() {
+  revealCard(3);
+});
+
+c4.addEventListener("click", function() {
+  revealCard(4);
+});
+c5.addEventListener("click", function() {
+  revealCard(5);
+});
+c6.addEventListener("click", function() {
+  revealCard(6);
+});
+c7.addEventListener("click", function() {
+  revealCard(7);
+});
+
+c8.addEventListener("click", function() {
+  revealCard(8);
+});
+c9.addEventListener("click", function() {
+  revealCard(9);
+});
+c10.addEventListener("click", function() {
+  revealCard(10);
+});
+c11.addEventListener("click", function() {
+  revealCard(11);
+});
+
+var onevisible = false;
+var turnCounter = 0;
+var visible_nr;
+var lock = false;
+var pairsleft = 6;
+
+function revealCard(nr) {
+  //alert(nr);
+  var opacityvalue = $("#c" + nr).css("opacity");
+  if (opacityvalue != 0 && lock == false) {
+    lock = true;
+    var obraz = "url(img/" + cards[nr] + ")";
+    $("#c" + nr).css("background-image", obraz);
+    $("#c" + nr).addClass("cardA");
+    $("#c" + nr).removeClass("card");
+
+    if (onevisible == false) {
+      //first card
+      onevisible = true;
+      visible_nr = nr;
+      lock = false;
+    } else {
+      //second card
+      if (cards[visible_nr] == cards[nr]) {
+        setTimeout(function() {
+          hide2cards(nr, visible_nr);
+        }, 800);
+      } else {
+        setTimeout(function() {
+          restore2cards(nr, visible_nr);
+        }, 800);
+      }
+      turnCounter++;
+      $(".score").html("Turn counter: " + turnCounter);
+      onevisible = false;
+    }
+  }
+}
+
+function hide2cards(nr1, nr2) {
+  $("#c" + nr1).css("opacity", "0");
+  $("#c" + nr2).css("opacity", "0");
+
+  pairsleft--;
+  if (pairsleft == 0) {
+    $(".board").html("<h1>You win !</br>Done in " + turnCounter + "turns</h1>");
+  }
+  lock = false;
+}
+
+function restore2cards(nr1, nr2) {
+  $("#c" + nr1).css("background-image", "url(img/karta.png");
+  $("#c" + nr1).addClass("card");
+  $("#c" + nr1).removeClass("cardA");
+
+  $("#c" + nr2).css("background-image", "url(img/karta.png");
+  $("#c" + nr2).addClass("card");
+  $("#c" + nr2).removeClass("cardA");
+  lock = false;
+}
